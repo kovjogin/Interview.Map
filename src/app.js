1cd83754b16019391e5ead52c5f3328e4d4d4d60
Роман Ковжогин <r.kovjogin@gmail.com>
2017-10-08 17:19:30 +0300

    clear app.js

--- src/app.js
+++ src/app.js
@@ -3,8 +3,6 @@ import  GoogleMapsLoader from 'google-maps';
 import { mapComponent } from './components/map/map.js';
 import { autocompleteComponent } from './components/autocomplete/autocomplete.js';
 import { markersComponent } from './components/markers/markers.js';
-import { emitter } from './utils/EventEmitter.js';
-
 
 GoogleMapsLoader.KEY = 'AIzaSyDgO6WEe8Na5ZwAemVtvZK8CKUt6KgPTcM';
 GoogleMapsLoader.LANGUAGE = 'ru';
@@ -14,17 +12,3 @@ GoogleMapsLoader.REGION = 'RU';
 new mapComponent(GoogleMapsLoader);
 new markersComponent();
 new autocompleteComponent();
-
-
-
-// map.addEventListener('click', () => {
-//     emitter.emit('event:name-changed', {name: 'test'});
-// });
-//
-// emitter.subscribe('event:name-changed', data => {
-//     console.log(`Your name is: ${data.name}`);
-// });
-
-
-
-//AIzaSyDgO6WEe8Na5ZwAemVtvZK8CKUt6KgPTcM
